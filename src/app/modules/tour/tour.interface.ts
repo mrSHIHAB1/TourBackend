ecd1a35565c7eb8ca75cca5a5a284b11852cb1bc
mrSHIHAB1 <montasirr36@gmail.com>
2025-07-25 16:31:38 +0600

    email validation

--- src/app/modules/tour/tour.interface.ts
+++ src/app/modules/tour/tour.interface.ts
@@ -11,6 +11,8 @@ export interface ITour {
     location?: string;
     costFrom?: number;
     startDate?: Date
+    departureLocation?: string;
+    arrivalLocation?: string;
     endDate?: Date;
     included?: string[];
     excluded?: string[]
@@ -20,4 +22,5 @@ export interface ITour {
     minAge?: number;
     division: Types.ObjectId
     tourType: Types.ObjectId
+    deleteImages?: string[]
 }
